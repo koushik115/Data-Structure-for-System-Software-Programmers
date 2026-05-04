f2ccdfa163143018bd16a3eb548f8eeba53754ce
Koushik <kaushikbhat115@gmail.com>
2026-05-04 21:43:03 +0530

    Add recursive utility functions and demos
    Introduce multiple recursive helpers to Recursion/recursion.c: convert (base conversion), sumdigits, power, PFactors (prime factorization), gcd, fib, divisibleBy9, divisibleBy11, and towerOfHanoi. Update main to demonstrate these functions with example calls. The compiled executable (Recursion/recursion) was also updated.

--- Recursion/recursion.c
+++ Recursion/recursion.c
@@ -39,6 +39,106 @@ int rseries(int n) {
 	return sum;
 }
 
+void convert(int num, int base) {
+	if(num == 0)
+		return;
+
+	int rem = num % base;
+
+	convert(num / base, base);
+
+	if(rem < 10)
+		printf("%d", rem);
+	else
+		printf("%c", rem - 10 + 'A');
+}
+
+int sumdigits(int n) {
+	if(n == 0)
+		return 0;
+
+	return (n % 10) + sumdigits(n / 10);
+}
+
+float power(int a, int n) {
+	if(n == 0)
+		return 1;
+
+	return a * power(a, n - 1);
+}
+
+void PFactors(int num) {
+	int i = 2;
+
+	if(num == 0 || num == 1)
+		return;
+
+	while(num % i != 0)
+		i++;
+
+	printf("%d ", i);
+	PFactors(num / i);
+}
+
+int gcd(int a, int b) {
+	if(b == 0)
+		return a;
+
+	return gcd(b, a % b);
+}
+
+int fib(int n) {
+	if(n == 0 || n == 1)
+		return 1;
+
+	return fib(n - 1) + fib(n - 2);
+}
+
+int divisibleBy9(long int n) {
+	if(n == 9)
+		return 1;
+	else if(n < 9)
+		return 0;
+
+	int sumDigits = 0;
+	while(n > 0) {
+		sumDigits = sumDigits + n % 10;
+		n = n / 10;
+	}
+
+	return divisibleBy9(sumDigits);
+}
+
+int divisibleBy11(long int n) {
+	int s1 = 0, s2 = 0, diff = 0;
+	if(n == 0)
+		return 1;
+	else if(n < 10)
+		return 0;
+
+	while(n > 0) {
+		s1 += n % 10;
+		n /= 10;
+		s2 += n % 10;
+		n /= 10;
+	}
+
+	diff = (s1 > s2) ? (s1 - s2) : (s2 - s1);
+	return divisibleBy11(diff);
+}
+
+void towerOfHanoi(int ndisk, char source, char temp, char dest) {
+	if(ndisk == 1) {
+		printf("Move Disk %d from %c ---> %c\n", ndisk, source, dest);
+		return;
+	}
+
+	towerOfHanoi(ndisk - 1, source, dest, temp);
+	printf("Move Disk %d from %c --> %c\n", ndisk, source, dest);
+	towerOfHanoi(ndisk - 1, temp, source, dest);
+}
+
+	
 int main(void) {
 /*
 	printf("%ld ", fact(3));
@@ -47,13 +147,32 @@ int main(void) {
 	printf("%d", summation(4));
 	printf("\n");
 
-	display1(5);
+                                                                                                	display1(5);
 	printf("\n");
 
 	display2(5);
 	printf("\n");
-*/
+
 	printf("\b\b = %d\n", rseries(5));
+
+	printf("%d\n", sumdigits(45329));
+
+	convert(15, 16);
+	printf("\n");
+
+	printf("%0.f\n", power(2, 11));
+
+	PFactors(84);
+	printf("\n");
+
+	printf("%d\n", fib(5));
+
+	printf("%d\n", divisibleBy9(1469358));
+
+	printf("%d\n", divisibleBy11(62938194));
+*/
+	towerOfHanoi(3, 'A', 'B', 'C');
+
 	return 0;
 }
 
